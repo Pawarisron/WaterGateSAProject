7a7283685642d77e1d64da28377ec5a2e8625921
Towie Montana <naradon.d@ku.th>
2023-11-05 22:05:30 +0700

    make daily report check gate_status if the value is 2

--- controller/employee-wg-reporter-controller.php
+++ controller/employee-wg-reporter-controller.php
@@ -42,7 +42,28 @@
                     $check_data->bindParam(':report_date', $report_date, PDO::PARAM_STR);
                     $check_data->execute();
 
-                    header('location: ../view/employee/employee-wg-reporter.php');
+                    try{
+                        $criterion_query = "SELECT criterion FROM watergate WHERE watergate_ID = :watergate_ID";
+                        $criterion_statement = $conn->prepare($criterion_query);
+                        $criterion_statement->bindParam(':watergate_ID', $watergate_ID, PDO::PARAM_STR);
+                        $criterion_statement->execute();
+
+                        $criterion_result = $criterion_statement->fetch(PDO::FETCH_ASSOC);
+                        $gate_status = ($upstream <= $criterion) ? 0 : 1;
+
+                        $update_gate_status_query = "UPDATE watergate SET gate_status = :gate_status WHERE watergate_ID = :watergate_ID";
+                        $update_gate_status_statement = $conn->prepare($update_gate_status_query);
+                        $update_gate_status_statement->bindParam(':gate_status', $gate_status, PDO::PARAM_INT);
+                        $update_gate_status_statement->bindParam(':watergate_ID', $watergate_ID, PDO::PARAM_STR);
+                        $update_gate_status_statement->execute();
+    
+                        header('location: ../view/employee/employee-wg-reporter.php');
+                    }
+                    catch(PDOException $e){
+                        echo $e->getMessage();
+    
+                    }
+                    
                 }
                 catch(PDOException $e){
                     echo $e->getMessage();
